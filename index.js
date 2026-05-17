cf606f5eea6637eee418fbcd1d2ac4e952c967b0
zDrowzy <drowzyontp@gmail.com>
2026-05-17 17:24:53 -0300

    Set up basic HTTP server in index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const http = require('http')
+
+http.createServer(function(req, res) {
+res.write('online')
+res.end()
+}).listen(process.env.PORT || 3000)
+
 const mineflayer = require('mineflayer')
 
 const SERVER_HOST = 'celestiansmp.us'
